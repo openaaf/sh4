a5700fc0aa002abedffe45dbe4b6c6577698c44a
obi <_obiwan_@web.de>
2025-11-02 17:02:03 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+
+FORMAT="[${0##*/}]"
 
 DIR=`pwd`
 GITPULL="git pull"
@@ -11,64 +14,86 @@ case "$1" in
 	clone)
 #		if [ "$(whoami)" == "obiaus" ];then
 		if [ ! -z "$SH4GIT" ] && [ "$SH4GIT" == "2" ];then
-			echo "git clone https://github.com/openaaf/buildsystem"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT git clone https://github.com/openaaf/buildsystem"
 			git clone https://github.com/openaaf/buildsystem buildsystem-ddt
 		else
-			echo "git clone https://github.com/openaaf/buildsystem-ddt"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT git clone https://github.com/openaaf/buildsystem-ddt"
 			git clone https://github.com/openaaf/buildsystem-ddt buildsystem-ddt
 		fi
-		echo "link cdk > buildsystem-ddt"
+
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT link cdk > buildsystem-ddt"
 		ln -s buildsystem-ddt cdk
 
 		cd $DIR/buildsystem-ddt
 
-		echo "git clone https://github.com/Duckbox-Developers/apps"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git clone https://github.com/Duckbox-Developers/apps"
 		git clone https://github.com/Duckbox-Developers/apps apps
-		echo "link apps > buildsystem-ddt/apps"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT link apps > buildsystem-ddt/apps"
 		ln -s buildsystem-ddt/apps ../apps
 
 		if [ "$(whoami)" == "obi" ];then
 #		if [ ! -z "$SH4GIT" ] && [ "$SH4GIT" == "2" ];then
-#			echo "git clone https://github.com/openaaf/driver"
+		    echo "$FORMAT -------------------------------------------"
+#			echo "$FORMAT git clone https://github.com/openaaf/driver"
 #			git clone https://github.com/openaaf/driver driver
-			echo "git clone https://github.com/openaaf/driver_ddt"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT git clone https://github.com/openaaf/driver_ddt"
 			git clone https://github.com/openaaf/driver_ddt driver
 		else
-			echo "git clone https://github.com/Duckbox-Developers/driver"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT git clone https://github.com/Duckbox-Developers/driver"
 			git clone https://github.com/Duckbox-Developers/driver driver
 		fi
-		echo "link driver > buildsystem-ddt/driver"
+
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT link driver > buildsystem-ddt/driver"
 		ln -s buildsystem-ddt/driver ../driver
 
-		echo "git clone https://github.com/Duckbox-Developers/flash"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git clone https://github.com/Duckbox-Developers/flash"
 		git clone https://github.com/Duckbox-Developers/flash flash
-		echo "link flash > buildsystem-ddt/flash"
+
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT link flash > buildsystem-ddt/flash"
 		ln -s buildsystem-ddt/flash ../flash
 
-		echo "link tufsbox > buildsystem-ddt/tufsbox"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT link tufsbox > buildsystem-ddt/tufsbox"
 		ln -s buildsystem-ddt/tufsbox ../tufsbox
 
 #		if [ "$(whoami)" == "obi" ];then
 		if [ ! -z "$SH4GIT" ] && [ "$SH4GIT" == "2" ];then
 			mv root/boot root/boot_org
-			echo "svn co --username buildbin --password buildbin http://svn.dyndns.tv/svn/tools/boot"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT svn co --username buildbin --password buildbin http://svn.dyndns.tv/svn/tools/boot"
 			svn co --username buildbin --password buildbin http://svn.dyndns.tv/svn/tools/boot root/boot
-			echo "add dummy root/boot/audio_7109.elf"
+		    echo "$FORMAT -------------------------------------------"
+			echo "$FORMAT add dummy root/boot/audio_7109.elf"
 			touch root/boot/audio_7109.elf
 		fi
 		;;
 	pull)
 		cd $DIR/buildsystem-ddt
-		echo "git pull buildsystem-ddt"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git pull buildsystem-ddt"
 		git pull
+	    echo "$FORMAT -------------------------------------------"
 		cd $DIR/buildsystem-ddt/apps
-		echo "git pull buildsystem-ddt/apps"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git pull buildsystem-ddt/apps"
 		git pull
 		cd $DIR/buildsystem-ddt/driver
-		echo "git pull buildsystem-ddt/driver"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git pull buildsystem-ddt/driver"
 		git pull
 		cd $DIR/buildsystem-ddt/flash
-		echo "git pull buildsystem-ddt/flash"
+	    echo "$FORMAT -------------------------------------------"
+		echo "$FORMAT git pull buildsystem-ddt/flash"
 		git pull
 		;;
 esac
